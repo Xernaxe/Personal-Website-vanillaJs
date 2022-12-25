9b86e75333559aeea2c7f34427382b78c1e70515
Xernaxe <93713092+Xernaxe@users.noreply.github.com>
2022-12-25 20:37:02 +0100

    Gameplay | (move left) implemented

--- assets/playground/game2048/game2048.js
+++ assets/playground/game2048/game2048.js
@@ -1,4 +1,4 @@
-const map = [
+let map = [
 	[0, 0, 0, 0],
 	[0, 0, 0, 0],
 	[0, 0, 0, 0],
@@ -7,19 +7,75 @@ const map = [
 
 const gameSection = document.querySelector('.game2048');
 
-function gameLoop() {
+function generateBoard() {
 	for (let i = 0; i < map.length; i++) {
 		for (let j = 0; j < map[i].length; j++) {
-			gameSection.innerHTML += `<div class="cell" data-value="0" data-position="${i} x ${j}">0</div>`;
+			gameSection.innerHTML += `<div class="cell" id="${i}${j}" data-value="0" data-position="${i} x ${j}"></div>`;
 		}
 	}
-  generate2StartingTiles()
+}
+function gameLoop() {
+	generateBoard();
+	generate2StartingTiles();
 }
 
-function reasignPosition() {
+function updateBoard() {
 	for (let i = 0; i < map.length; i++) {
 		for (let j = 0; j < map[i].length; j++) {
-			gameSection.innerHTML += `<div class="cell" data-value="0" data-position="${i} x ${j}"></div>`;
+			const cell = document.getElementById(`${i}${j}`);
+			cell.dataset.value = map[i][j];
+			cell.dataset.position = `${i} x ${j}`;
+			cell.innerText = map[i][j];
+
+			switch (cell.dataset.value) {
+				case '0':
+					cell.className = 'cell';
+					break;
+				case '2':
+					cell.className = 'cell';
+					cell.classList.toggle('cellx2');
+					break;
+				case '4':
+					cell.className = 'cell';
+					cell.classList.toggle('cellx4');
+					break;
+				case '8':
+					cell.className = 'cell';
+					cell.classList.toggle('cellx8');
+					break;
+				case '16':
+					cell.className = 'cell';
+					cell.classList.toggle('cellx16');
+					break;
+				case '32':
+					cell.className = 'cell';
+					cell.classList.toggle('cellx32');
+					break;
+				case '64':
+					cell.className = 'cell';
+					cell.classList.toggle('cellx64');
+					break;
+				case '128':
+					cell.className = 'cell';
+					cell.classList.toggle('cellx128');
+					break;
+				case '256':
+					cell.className = 'cell';
+					cell.classList.toggle('cellx256');
+					break;
+				case '512':
+					cell.className = 'cell';
+					cell.classList.toggle('cellx512');
+					break;
+				case '1024':
+					cell.className = 'cell';
+					cell.classList.toggle('cellx1024');
+					break;
+				case '2048':
+					cell.className = 'cell';
+					cell.classList.toggle('cellx2048');
+					break;
+			}
 		}
 	}
 }
@@ -27,15 +83,83 @@ function reasignPosition() {
 function generateStartingTile() {
 	let randomNr1 = Math.floor(Math.random() * 4);
 	let randomNr2 = Math.floor(Math.random() * 4);
-	let randomValue = Math.floor(Math.random() * 2);
-	// console.log(map[randomNr1][randomNr2]);
-  map[randomNr1][randomNr2] = randomValue * 2
-  // console.log(map);
+	console.log(`${randomNr1} x ${randomNr2} "tile"`);
+	let randomValue = Math.floor(Math.random() * 2 + 1);
+	map[randomNr1][randomNr2] = randomValue * 2;
 }
+
 function generate2StartingTiles() {
 	generateStartingTile();
 	generateStartingTile();
-  console.log(map);
+	generateStartingTile();
+	generateStartingTile();
+	generateStartingTile();
+	generateStartingTile();
+	generateStartingTile();
+	generateStartingTile();
+	updateBoard();
+	console.log(map);
 }
 
 gameLoop();
+
+window.addEventListener('keydown', (e) => {
+	console.log(e.key);
+
+	switch (e.key) {
+		case 'a':
+			shiftLeft();
+			break;
+
+		case 'd':
+			shiftRight();
+			break;
+
+		case 'w':
+			shiftUp();
+			break;
+
+		case 's':
+			shiftDown();
+			break;
+	}
+});
+
+function shiftLeft() {
+	// mapa provizorie
+	let newMap = [];
+
+	//parse prin tot map-u pentru randuri
+	for (let i = 0; i < map.length; i++) {
+		// populeaza un nou array cu valori care nu sunt egale cu 0
+		let newArr = map[i].filter((item) => item != 0);
+		// verifica daca noul array are cel putin un element
+		if (newArr.length) {
+			// parseaza noul array incepand de la index-
+			for (let k = 0; k < newArr.length; k++) {
+				if (newArr[k] === newArr[k + 1]) {
+					newArr[k] = newArr[k] + newArr[k + 1];
+					newArr[k + 1] = 0;
+					newArr = newArr.filter((item) => item != 0);
+					console.log(newArr + 'new arr');
+				}
+			}
+		}
+		fillArray(newArr);
+		newMap.push(newArr);
+	}
+	map = newMap;
+	updateBoard();
+}
+
+function shiftRight() {
+	// let newMap = []
+	// map.reverse()
+	// updateBoard()
+}
+
+function fillArray(arr) {
+	while (arr.length < 4) {
+		arr.push(0);
+	}
+}
