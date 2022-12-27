615358c5597ea2d77dd1bcdebbbac25daac08acb
Xernaxe <93713092+Xernaxe@users.noreply.github.com>
2022-12-27 05:31:23 +0100

    basic gameplay done

--- assets/playground/game2048/game2048.js
+++ assets/playground/game2048/game2048.js
@@ -4,8 +4,15 @@ let map = [
 	[0, 0, 0, 0],
 	[0, 0, 0, 0],
 ];
-
 const gameSection = document.querySelector('.game2048');
+const h2 = document.querySelector('.info2048');
+
+// let map = [
+// 	[2, 4, 8, 16],
+// 	[16, 8, 4, 2],
+// 	[2, 4, 8, 16],
+// 	[16, 8, 4, 2],
+// ];
 
 function generateBoard() {
 	for (let i = 0; i < map.length; i++) {
@@ -14,9 +21,22 @@ function generateBoard() {
 		}
 	}
 }
+
 function gameLoop() {
 	generateBoard();
 	generate2StartingTiles();
+	updateBoard();
+}
+
+function fillArray(arr) {
+	while (arr.length < 4) {
+		arr.push(0);
+	}
+}
+function unshiftArr(arr) {
+	while (arr.length < 4) {
+		arr.unshift(0);
+	}
 }
 
 function updateBoard() {
@@ -79,69 +99,68 @@ function updateBoard() {
 		}
 	}
 }
+function containsZeros(arr) {
+	return arr.some((row) => row.includes(0));
+}
 
 function generateStartingTile() {
-	let randomNr1 = Math.floor(Math.random() * 4);
-	let randomNr2 = Math.floor(Math.random() * 4);
-	console.log(`${randomNr1} x ${randomNr2} "tile"`);
-	let randomValue = Math.floor(Math.random() * 2 + 1);
-	map[randomNr1][randomNr2] = randomValue * 2;
+	if (containsZeros(map)) {
+		let randomNr1 = Math.floor(Math.random() * 4);
+		let randomNr2 = Math.floor(Math.random() * 4);
+		if (map[randomNr1][randomNr2] !== 0 && containsZeros(map)) {
+			generateStartingTile();
+		} else {
+			let randomValue = Math.floor(Math.random() * 2 + 1);
+			map[randomNr1][randomNr2] = randomValue * 2;
+		}
+	}
+	updateBoard();
 }
 
 function generate2StartingTiles() {
 	generateStartingTile();
 	generateStartingTile();
-	generateStartingTile();
-	generateStartingTile();
-	generateStartingTile();
-	generateStartingTile();
-	generateStartingTile();
-	generateStartingTile();
-	updateBoard();
-	console.log(map);
 }
 
-gameLoop();
-
 window.addEventListener('keydown', (e) => {
-	console.log(e.key);
-
 	switch (e.key) {
 		case 'a':
 			shiftLeft();
+			generateStartingTile();
 			break;
 
 		case 'd':
 			shiftRight();
+			generateStartingTile();
 			break;
 
 		case 'w':
 			shiftUp();
+			generateStartingTile();
 			break;
 
 		case 's':
+			gameOver()
 			shiftDown();
+			generateStartingTile();
 			break;
 	}
 });
 
 function shiftLeft() {
-	// mapa provizorie
 	let newMap = [];
-
-	//parse prin tot map-u pentru randuri
 	for (let i = 0; i < map.length; i++) {
-		// populeaza un nou array cu valori care nu sunt egale cu 0
 		let newArr = map[i].filter((item) => item != 0);
-		// verifica daca noul array are cel putin un element
 		if (newArr.length) {
-			// parseaza noul array incepand de la index-
 			for (let k = 0; k < newArr.length; k++) {
-				if (newArr[k] === newArr[k + 1]) {
+				if (
+					newArr[k] === newArr[k + 1] &&
+					newArr[k] !== 0 &&
+					newArr[k + 1] !== 0
+				) {
 					newArr[k] = newArr[k] + newArr[k + 1];
 					newArr[k + 1] = 0;
 					newArr = newArr.filter((item) => item != 0);
-					console.log(newArr + 'new arr');
 				}
 			}
 		}
@@ -153,13 +172,119 @@ function shiftLeft() {
 }
 
 function shiftRight() {
-	// let newMap = []
-	// map.reverse()
-	// updateBoard()
+	let newMap = [];
+	for (let i = 0; i < map.length; i++) {
+		let newArr = map[i].filter((item) => item != 0);
+		if (newArr.length) {
+			for (let k = newArr.length; k > 0; k--) {
+				if (newArr[k] === newArr[k - 1]) {
+					newArr[k] = newArr[k] + newArr[k - 1];
+					newArr[k - 1] = 0;
+					newArr = newArr.filter((item) => item != 0);
+				}
+			}
+		}
+		unshiftArr(newArr);
+		newMap.push(newArr);
+	}
+	map = newMap;
+	updateBoard();
 }
 
-function fillArray(arr) {
-	while (arr.length < 4) {
-		arr.push(0);
+function shiftUp() {
+	let newMap = [];
+	let newArr;
+	for (let i = 0; i < map.length; i++) {
+		newArr = [];
+		map.forEach((row) => {
+			newArr.push(row[i]);
+			newArr = newArr.filter((item) => item != 0);
+		});
+		if (newArr.length) {
+			for (let k = 0; k < newArr.length; k++) {
+				if (newArr[k] === newArr[k + 1]) {
+					newArr[k] = newArr[k] + newArr[k + 1];
+					newArr[k + 1] = 0;
+					newArr = newArr.filter((item) => item != 0);
+				}
+			}
+		}
+		fillArray(newArr);
+		newMap.push(newArr);
+	}
+
+	let dummyMap = [
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+	];
+
+	for (let i = 0; i < 4; i++) {
+		for (let b = 0; b < 4; b++) {
+			dummyMap[b][i] = newMap[i][b];
+		}
+	}
+	map = dummyMap;
+	updateBoard();
+}
+
+function shiftDown(checking) {
+	let newMap = [];
+	let newArr;
+	for (let i = 0; i < map.length; i++) {
+		newArr = [];
+		map.forEach((row) => {
+			newArr.push(row[i]);
+			newArr = newArr.filter((item) => item != 0);
+		});
+		if (newArr.length > 1) {
+			for (let k = newArr.length; k > 0; k--) {
+				if (newArr[k] === newArr[k - 1]) {
+					newArr[k] = newArr[k] + newArr[k - 1];
+					newArr[k - 1] = 0;
+					newArr = newArr.filter((item) => item != 0);
+					k--
+				}
+			}
+		}
+		unshiftArr(newArr);
+		newMap.push(newArr);
+	}
+
+	let dummyMap = [
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+	];
+
+	for (let i = 3; i >= 0; i--) {
+		for (let b = 0; b < map.length; b++) {
+			dummyMap[b][i] = newMap[i][b];
+		}
+	}
+	if(!checking){
+		map = dummyMap;
+		updateBoard();
 	}
 }
+const doesNotHave0 = (currentValue) => currentValue != 0;
+// GAME OVER CODE | STILL WORKING ON IT
+// faci o copie la harta, aplici shiftDown,up,left,right si compari daca sunt la fel
+// daca sunt la fel inseamna ca nu mai sunt mutari posibile
+function gameOver(){
+	let arr1dMap = [].concat.apply([], map);
+	let arr1dDummyMap = map;
+	shiftDown(arr1dDummyMap ,true)
+	arr1dDummyMap.concat.apply([], arr1dDummyMap)
+	console.log(arr1dMap);
+	console.log(arr1dDummyMap);
+	if(arr1dDummyMap == arr1dMap){
+		h2.innerText = "game over"
+	} else{
+		h2.innerText = "not over"
+	}
+}
+
+gameLoop();
